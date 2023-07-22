ed792cc62060b7879a91f30c5cdb1bac15ebf53e
Wayne Sun <wayne10032004@gmail.com>
2023-07-23 06:09:17 +0800

    fix:cartSeeder

--- seeders/20230721172706-cartSeeder.js
+++ seeders/20230721172706-cartSeeder.js
@@ -3,34 +3,75 @@ const faker = require('faker');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     try {
+      // Helper function to generate a random integer between min and max (inclusive)
+      function getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
       // Get all items and stocks
-      const items = await queryInterface.sequelize.query('SELECT id, price FROM Items;', {
+      const items = await queryInterface.sequelize.query("SELECT id, price FROM Items WHERE state <> 'false'", {
         type: queryInterface.sequelize.QueryTypes.SELECT,
       });
 
-      const stocks = await queryInterface.sequelize.query('SELECT id, itemStock, ItemId FROM Stocks;', {
+      const stocks = await queryInterface.sequelize.query("SELECT id, itemStock, ItemId FROM Stocks", {
         type: queryInterface.sequelize.QueryTypes.SELECT,
       });
+
+      const users = await queryInterface.sequelize.query(
+        "SELECT id FROM Users WHERE role <> 'seller'",
+        { type: queryInterface.sequelize.QueryTypes.SELECT }
+      );
 
       const fakeCarts = [];
 
-      for (const stock of stocks) {
-        const item = items.find((item) => item.id === stock.ItemId);
-        if (!item) {
+      function cartExists(userId, stockId, usedStockIds) {
+        return fakeCarts.some((cart) => cart.UserId === userId && cart.StockId === stockId) ||
+          usedStockIds[userId]?.has(stockId);
+      }
+
+      for (let i = 0; i < 10; i++) {
+        let randomItem, randomStock, itemQuantity, amount, randomUser;
+
+        const usedStockIds = {};
+
+        do {
+          // Randomly select a user
+          randomUser = faker.random.arrayElement(users);
+
+          // If there are no stocks available for the user, skip generating the cart for this user
+          const userStocks = stocks.filter((stock) => stock.ItemId !== 'false' && !cartExists(randomUser.id, stock.id, usedStockIds));
+          if (userStocks.length === 0) {
+            console.log('No more valid stocks for user:', randomUser);
+            break;
+          }
+
+          // Randomly select an item and stock
+          randomItem = faker.random.arrayElement(items);
+          randomStock = userStocks.find((stock) => stock.ItemId === randomItem.id);
+        } while (!randomStock); // Keep re-selecting until a valid stock is found
+
+        if (!randomStock) {
+          console.log('No stock found for user:', randomUser);
           continue;
         }
 
-        const itemQuantity = faker.datatype.number({ min: 1, max: stock.itemStock });
-        const amount = item.price * itemQuantity;
-        const orderId = faker.datatype.number({ min: 1, max: 100 });
-        const userId = faker.datatype.number({ min: 1, max: 1000 }); // Generating a random UserId between 1 and 1000
+        // Generate a random quantity for the cart (between 1 and itemStock)
+        itemQuantity = getRandomInteger(1, randomStock.itemStock);
+
+        // Calculate the amount (price * itemQuantity)
+        amount = randomItem.price * itemQuantity;
+
+        // Store the used StockId for the selected user
+        if (!usedStockIds[randomUser.id]) {
+          usedStockIds[randomUser.id] = new Set();
+        }
+        usedStockIds[randomUser.id].add(randomStock.id);
 
         fakeCarts.push({
-          stockId: stock.id,
-          UserId: userId,
-          itemQuantity,
-          amount,
-          OrderId: orderId,
+          StockId: randomStock.id, // Assuming the column name is 'StockId' in the Carts table
+          UserId: randomUser.id,
+          itemQuantity: itemQuantity,
+          amount: amount,
           createdAt: new Date(),
           updatedAt: new Date(),
         });
